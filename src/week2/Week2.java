a7e31db926aeecf9d780f09ccf425c0aa6e8da14
Barxl <lucasbarbosaemail@gmail.com>
2022-10-01 16:02:56 +0100

    ff

--- src/week2/Week2.java
+++ src/week2/Week2.java
@@ -15,9 +15,9 @@ public class Week2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //ddffgg
+        
         System.out.println("ggkkjj");
-        System.out.println("");
+        System.out.println("lucas");
     }
     
 }
